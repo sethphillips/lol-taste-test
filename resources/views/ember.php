9e3a772cb324444db9054e31581208998c318c45
Seth Phillips <seth.phillips@trms.com>
2016-07-10 09:23:45 -0500

    make scan modal close with any click

--- resources/views/ember.php
+++ resources/views/ember.php
@@ -8,10 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <base href="/" />
-<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+8ebdfcf6%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
+<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+b691868e%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
 
     <link rel="stylesheet" href="assets/vendor-4ab6d985bd04fd3b3ab3670454af5ff9.css">
-    <link rel="stylesheet" href="assets/lol-taste-test-1efcc16f9928e59e81532b25b1d29521.css">
+    <link rel="stylesheet" href="assets/lol-taste-test-e92acbe465b4302d25489cd7a52c3cf6.css">
 
     
   </head>
@@ -19,7 +19,7 @@
     
 
     <script src="assets/vendor-17e623cf844cbb41bbb6941bf22b1a52.js"></script>
-    <script src="assets/lol-taste-test-373ce1bef072c8757fb2f537e9cb8dd5.js"></script>
+    <script src="assets/lol-taste-test-b53c97d546c8392d5005286ccf0b58e2.js"></script>
 
     <div id="ember-basic-dropdown-wormhole"></div>
   </body>
