0faf01e8a8ee07a9ea618273941ca1222c8bc7a2
Seth Phillips <seth.phillips@trms.com>
2017-06-20 10:47:11 -0500

    remove arrayrandom util ref

--- ember/app/index/controller.js
+++ ember/app/index/controller.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import {task, timeout} from 'ember-concurrency';
-import arrayRandom from 'lol-taste-test/utils/array-random';
 
 export default Ember.Controller.extend({
 
