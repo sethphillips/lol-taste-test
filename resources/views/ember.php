324b6bbb45e2c018c4c312445f00b114e64a3195
Seth Phillips <seth.phillips@trms.com>
2017-06-21 22:14:26 -0500

    fix start image and build to server

--- resources/views/ember.php
+++ resources/views/ember.php
@@ -8,18 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <base href="/" />
-<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+f9cd5b2e%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
+<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+b72fa644%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
 
-    <link rel="stylesheet" href="assets/vendor-4ab6d985bd04fd3b3ab3670454af5ff9.css">
-    <link rel="stylesheet" href="assets/lol-taste-test-990dd1fab06f6d24a55ab964db35e9da.css">
+    <link rel="stylesheet" href="assets/vendor-9bb9754dbfbe5c858bb0428ea0f1a6d5.css">
+    <link rel="stylesheet" href="assets/lol-taste-test-5ec4466329ef4c35109dc854d841efe8.css">
 
     
   </head>
   <body>
     
 
-    <script src="assets/vendor-17e623cf844cbb41bbb6941bf22b1a52.js"></script>
-    <script src="assets/lol-taste-test-b7a1ec42f5e768d8a76b91a0adbbc6f0.js"></script>
+    <script src="assets/vendor-bad48d6b5438332d068eb66d61e91d74.js"></script>
+    <script src="assets/lol-taste-test-c1464f8b164ecb305d1c9c271b114933.js"></script>
 
     <div id="ember-basic-dropdown-wormhole"></div>
   </body>
