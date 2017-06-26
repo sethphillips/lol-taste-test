e9d82842d27ffbc6cb0c8ebe18341983b0f64652
Seth Phillips <seth.phillips@trms.com>
2017-06-26 11:01:12 -0500

    update signup screensaver

--- resources/views/ember.php
+++ resources/views/ember.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <base href="/" />
-<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+a5fe8e96%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
+<meta name="lol-taste-test/config/environment" content="%7B%22modulePrefix%22%3A%22lol-taste-test%22%2C%22environment%22%3A%22production%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22lol-taste-test%22%2C%22version%22%3A%220.0.0+bd4b6907%22%7D%2C%22i18n%22%3A%7B%22defaultLocale%22%3A%22en%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />
 
     <link rel="stylesheet" href="assets/vendor-9bb9754dbfbe5c858bb0428ea0f1a6d5.css">
     <link rel="stylesheet" href="assets/lol-taste-test-6f22c78a735eaf4522a2df8a7d543881.css">
@@ -19,7 +19,7 @@
     
 
     <script src="assets/vendor-bad48d6b5438332d068eb66d61e91d74.js"></script>
-    <script src="assets/lol-taste-test-515864e476386a6975e80d07eee1c2e2.js"></script>
+    <script src="assets/lol-taste-test-b976d7708569f46824b17558e2a2256e.js"></script>
 
     <div id="ember-basic-dropdown-wormhole"></div>
   </body>
